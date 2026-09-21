85791216b608616777cec28fe5d7170ea1107a68
Shreyas H N <shreyashn899@gmail.com>
2026-09-21 19:42:45 +0530

    Time: 0 ms (100%), Space: 19.4 MB (55.66%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,24 +1,50 @@
 class MyQueue:
 
     def __init__(self):
-        self.queue = []
-        self.stack=[]
-    def push(self, y):
-        self.queue.append(y)
+        self.stack1=[]
+        self.stack2=[]
 
-    def pop(self):
-        if not self.stack:
-            while self.queue:
-                self.stack.append(self.queue.pop())
-        return self.stack.pop()
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+        
 
+    def pop(self) -> int:
+        if self.stack2:
+            x=self.stack2.pop()
+            return x
+        else:
+            while self.stack1:
+                if len(self.stack1)==1:
+                    return self.stack1.pop()
+                x=self.stack1.pop()
+                self.stack2.append(x)
+            return None
 
-    def peek(self):
-        if not self.stack:
-            while self.queue:
-                self.stack.append(self.queue.pop())
-        return self.stack[-1]
+        
 
+    def peek(self) -> int:
+        if self.stack2:
+            return self.stack2[-1]
+        else:
+            while self.stack1:
+                x=self.stack1.pop()
+                self.stack2.append(x)
+        return self.stack2[-1]
+
+        
 
     def empty(self) -> bool:
-        return len(self.stack)==0 and len(self.queue)==0
+        if len(self.stack2)==0 and len(self.stack1)==0:
+            return True
+        else:
+            return False
+            
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
